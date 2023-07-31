5e00c94c8182ae814cc71ecaa69842d39642907a
johnie261 <njorogejohn261@gmail.com>
2023-07-31 14:32:24 +0300

    Set up thirdweb and also react

---
+++ client/src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Routes, Route} from 'react-router-dom'
+
+import { CampaignDetails, CreateCampaign, Home, Profile } from './pages';
+import { Navbar, Sidebar } from './components';
+
+const App = () => {
+  return (
+    <div className="relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row">
+      <div className="sm:flex hidden mr-10 relative">
+        <Sidebar />
+      </div>
+      <div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5">
+        <Navbar />
+      </div>
+        
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
